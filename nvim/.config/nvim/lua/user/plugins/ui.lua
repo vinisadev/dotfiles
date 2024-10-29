10c8f358d59e6d9b265da984ac3fcf1a1b433a65
Vincenzo Fehring <vinfehring@gmail.com>
2024-10-29 12:31:41 -0500

    [neovim] Add ui dashboard

--- nvim/.config/nvim/lua/user/plugins/ui.lua
+++ nvim/.config/nvim/lua/user/plugins/ui.lua
@@ -37,4 +37,47 @@ return {
       char = "▏",
     },
   },
+
+    -- dashboard
+    {
+      "goolord/alpha-nvim",
+      lazy = false,
+      config = function()
+        local dashboard = require("alpha.themes.dashboard")
+        local logo = [[
+██████╗  ██████╗ ███╗   ███╗ █████╗ ██╗███╗   ██╗███████╗ ██████╗ 
+██╔══██╗██╔═══██╗████╗ ████║██╔══██╗██║████╗  ██║╚════██║██╔════╝ 
+██║  ██║██║   ██║██╔████╔██║███████║██║██╔██╗ ██║    ██╔╝███████╗ 
+██║  ██║██║   ██║██║╚██╔╝██║██╔══██║██║██║╚██╗██║   ██╔╝ ██╔═══██╗
+██████╔╝╚██████╔╝██║ ╚═╝ ██║██║  ██║██║██║ ╚████║   ██║  ╚██████╔╝
+╚═════╝  ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝   ╚═╝   ╚═════╝ 
+  ]]
+
+        dashboard.section.header.val = vim.split(logo, "\n")
+        dashboard.section.buttons.val = {
+          dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+          dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+          dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+          dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
+          dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+          dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+        }
+        dashboard.section.footer.opts.hl = "Type"
+        dashboard.section.header.opts.hl = "Include"
+        dashboard.section.buttons.opts.hl = "Keyword"
+        dashboard.opts.opts.noautocmd = true
+
+        require("alpha").setup(dashboard.opts)
+
+        vim.api.nvim_create_autocmd("User", {
+          pattern = "LazyVimStarted",
+          callback = function()
+            local stats = require("lazy").stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            dashboard.section.footer.val = "🎉 Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
+            pcall(vim.cmd.AlphaRedraw)
+          end,
+        })
+      end,
+    },
 }
