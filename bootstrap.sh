5209e50a485891e32b0e1d0631092584426a1dda
Vincenzo Fehring <vinfehring@gmail.com>
2024-10-29 11:28:01 -0500

    [bootstrap] Add docker and docker-compose to essential packages | Perform post install configuration for docker

--- bootstrap.sh
+++ bootstrap.sh
@@ -67,6 +67,8 @@ fi
 # Install essential packages
 PACKAGES=(
   "discord"
+  "docker"
+  "docker-compose"
   "godot"
   "go"
   "libreoffice-still"
@@ -142,6 +144,30 @@ print_color "Installing latest Node.js version..." "$YELLOW"
 
   print_color "Node.js version: $(node --version)" "$GREEN" # Print the installed Node.js version
   print_color "npm version: $(npm --version)" "$GREEN" # Print the installed npm version
+)
+
+# Docker configuration in a subshell
+(
+  print_color "Configuring Docker..." "$YELLOW"
+
+  # Check if Docker is installed
+  if command_exists docker; then
+    # Post install configuration for Docker
+    sudo usermod -aG docker $USER
+
+    # Enable docker service
+    print_color "Enabling docker service..." "$YELLOW"
+    sudo systemctl enable docker
+
+    # Start docker service
+    print_color "Starting docker service..." "$YELLOW"
+    sudo systemctl start docker
+
+    print_color "Docker configuration completed." "$GREEN"
+    print_color "Please log out and log back in for Docker group changes to take effect." "$YELLOW"
+  else
+    print_color "Docker is not installed. Skipping Docker configuration." "$RED"
+  fi
 )
 
 # Install AUR helper (paru)
