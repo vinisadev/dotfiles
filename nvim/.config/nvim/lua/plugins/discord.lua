1b606d54ac07b1733eb5e6251e8d37c57c6ea746
Vincenzo Fehring <vinfehring@gmail.com>
2024-12-03 19:02:56 -0600

    [neovim] Set discord rich presence to default configuration

--- nvim/.config/nvim/lua/plugins/discord.lua
+++ nvim/.config/nvim/lua/plugins/discord.lua
@@ -2,10 +2,6 @@ return {
   "andweeb/presence.nvim",
   lazy = false,
   config = function()
-    require("presence").setup({
-      editing_text = "Making %s my bitch!",
-      workspace_text = "I use Arch by the way...",
-      line_number_text = "Line %s out of %s.. This is crazy",
-    })
+    require("presence").setup({})
   end,
 }
